3421c853f3fe0e15cbbd7e02d31bdc1f8087bd79
Hoa Le <Hoa_le9485@yahoo.com>
2021-11-05 18:31:23 -0400

    commit tree c3267822e990a3de8b5b716975c5f96669e62758

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function superbowlWin(record) {
+    // const result = record.find(record => record.result === "W");
+    // return !!result ? result.year : undefined;
+    const result = record.find(record => record.result === "W")
+    if (result) {
+        return result.year
+    }
+}
+
+// !!result take in account of undefined
+// !result would only take in account of true false
+
+  //cant use record.result because its in array.
 
